3f3f2fc256119da27934da671ec46ce9c2cc2640
Jesse Sadler <jsadler@ucla.edu>
2020-07-11 18:21:38 -0700

    Package documentation
    * Create amganalysis-package.R for package documentation.
    * Move pipe operator usage to package documentation.
    * Tidy eval helpers in package documentation.

---
+++ R/amganalysis-package.R
@@ -0,0 +1,28 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+#' Tidy eval helpers
+#'
+#' @name tidyeval
+#' @keywords internal
+#' @importFrom rlang :=
+#' @importFrom rlang .data
+NULL
