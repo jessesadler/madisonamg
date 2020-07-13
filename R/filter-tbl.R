e08be28d3b66ee42e75268f5a830e5a9ae023cfd
Jesse Sadler <jsadler@ucla.edu>
2020-07-13 11:57:34 -0700

    Update filter-tbl documentation

--- R/filter-tbl.R
+++ R/filter-tbl.R
@@ -2,22 +2,37 @@
 
 #' Filter out all samples that are not a peak
 #'
-#' Use to make plot with just peaks
+#' `filter_peaks()` is useful to plot only the peaks from a trace.
 #'
 #' @inheritParams find_stimuli
 #' @inheritParams peaks_delay
-#' @param length_out Default is 200. The length of each peak in samples.
+#' @param length_out Default is 200. The length of each peak in samples. Use
+#'   `peaks_length()` to check length of peaks to find good value.
+#'
+#' @return A tibble with rows equal to `length_out * length(peaks)` and six
+#'   columns. `filter_peaks()` creates two new columns: a `sub_sample` column
+#'   that is `1:length_out` for each peak and a `peak_nr` to identify each
+#'   peak.
+#'
+#' @examples
+#' x <- find_peaks_response(ex_trace_tbl)
+#'
+#' peaks_length(x)
+#'
+#' filter_peaks(ex_trace_tbl, x, 250)
 #'
 #' @export
 
 filter_peaks <- function(df, peaks, length_out = 200) {
   peak_nr <- seq_along(peaks)
   # Figure out the number of samples to be added to each group to equal length_out
-  lengthen <- (length_out - purrr::map_dbl(peaks, length)) %/% 2
+  lengthen <- (length_out - peaks_length(peaks)) %/% 2
 
+  # Use below function to perform filtering and row bind with map2_df()
   purrr::map2_df(peak_nr, lengthen, ~ .filter_peaks(df, peaks, .x, .y, length_out))
 }
 
+# Function to perform the filtering and mutate
 .filter_peaks <- function(df, peaks, peak_nr, lengthen, length_out) {
   df %>%
     dplyr::filter(sample %in% seq(min(peaks[[peak_nr]]) - lengthen,
@@ -29,11 +44,11 @@ filter_peaks <- function(df, peaks, length_out = 200) {
 
 # Filter full response ----------------------------------------------------
 
-#' Filter data frame by stiumli
+#' Filter trace data frame by stiumli
 #'
 #' Filter out anything before the first stimulus and after the last stimulus.
-#'
-#' Used to plot full response.
+#' This gets rid of all recordings before and after the performance of the
+#' experiment. This is particularly useful for plotting the full trace.
 #'
 #' @inheritParams find_stimuli
 #' @inheritParams find_peaks_stimulus
@@ -41,6 +56,9 @@ filter_peaks <- function(df, peaks, length_out = 200) {
 #'   and after the last stimulus.
 #'
 #' @seealso `filter_full_response()`
+#'
+#' @examples
+#' filter_full_stimuli(ex_trace_tbl)
 #'
 #' @export
 
@@ -57,15 +75,19 @@ filter_full_stimuli <- function(df, buffer = 100,
 
 #' Filter data frame by response
 #'
-#' Filter out anything before the beginning of the first peak and after
-#' the end of the last peak.
-#'
-#' Used to plot full response.
+#' Filter out anything before the first peak and after the last peak.
+#' This gets rid of all recordings before and after the performance of the
+#' experiment. This is particularly useful for plotting the full trace.
 #'
 #' @inheritParams filter_full_stimuli
 #' @inheritParams peaks_checks
 #'
 #' @seealso `filter_full_stimuli()`
+#'
+#' @examples
+#' x <- find_peaks_response(ex_trace_tbl)
+#'
+#' filter_full_response(ex_trace_tbl, x)
 #'
 #' @export
 
