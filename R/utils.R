4f66554676609cdc50d956e30495abe0595a3f5c
Jesse Sadler <jsadler@ucla.edu>
2020-07-13 19:13:52 -0700

    Add buffer_df and conversion functions
    * Create buffer_df()
    * Functions to convert between samples and milliseconds.
    * Documentation for these internal functions.

--- R/utils.R
+++ R/utils.R
@@ -25,3 +25,47 @@ split_list <- function(values, index) {
   # Remove names
   purrr::set_names(out, NULL)
 }
+
+
+#' Convert between milliseconds and samples at a given frequency
+#'
+#' @param x Value to convert.
+#' @param freq Frequency of trace.
+#'
+#' @keywords internal
+#'
+#' @name ms_conversion
+NULL
+
+#' @rdname ms_conversion
+ms_to_samples <- function(x, freq) {
+  x * freq / 1000
+}
+
+#' @rdname ms_conversion
+samples_to_ms <- function(x, freq) {
+  x * 1000 / freq
+}
+
+#' Create a buffer data frame to lengthen a filtered trace
+#'
+#' Used in visualization functions.
+#'
+#' @param df A trace data frame.
+#' @param extent A vector of samples for the extent of filtered trace to which
+#'   the buffer is added. Usually this is where stimuli occur from
+#'   `stimuli_samples()`.
+#' @param buffer Buffer to add to the beginning and end of the plot in
+#'   milliseconds.
+#' @param freq Frequency of the trace recording.
+#'
+#' @keywords internal
+
+buffer_df <- function(df, extent, buffer, freq) {
+
+  buffer <- ms_to_samples(buffer, freq)
+  start <- min(extent) - buffer
+  end <- max(extent) + buffer
+
+  df <- dplyr::filter(df, .data$sample >= start & .data$sample <= end)
+}
